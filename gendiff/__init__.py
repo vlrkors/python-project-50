d9d358499c57b7215a78145f85908c32025481ca
vlrkors <vlrkors@gmail.com>
2025-08-20 18:07:26 +0300

    add new main

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,17 +1,5 @@
-"""Пакет для сравнения конфигурационных файлов."""
+from gendiff.scripts.generate_diff import generate_diff
 
-from gendiff.gendiff import generate_diff
-
-# gendiff/__init__.py
-from .parsers.json_parser import parse_json
-from .parsers.yaml_parser import parse_yaml
-
-FORMATS = {
-    'json': parse_json,
-    'yml': parse_yaml,
-    'yaml': parse_yaml,
-}
-
-__all__ = ["generate_diff"]
-__version__ = "0.1.1"
-
+__all__ = (
+    "generate_diff",
+)
